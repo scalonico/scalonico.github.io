8c0ea40a0d1caadebd18b82ef3bc81e90eb94f36
Sebastian Calonico <scalonico@ucdavis.edu>
2026-04-17 16:48:09 -0700

    Add GA4 tracking and tighten CV header

---
+++ analytics.js
@@ -0,0 +1,24 @@
+const GA_MEASUREMENT_ID = "G-NCLZM2KWSY";
+
+if (GA_MEASUREMENT_ID !== "G-XXXXXXXXXX") {
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+
+  window.gtag = gtag;
+  gtag("js", new Date());
+  gtag("config", GA_MEASUREMENT_ID);
+
+  const gaScript = document.createElement("script");
+  gaScript.async = true;
+  gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(
+    GA_MEASUREMENT_ID
+  )}`;
+  document.head.appendChild(gaScript);
+} else {
+  console.warn(
+    "Google Analytics is not active yet. Replace G-XXXXXXXXXX in analytics.js with your GA4 measurement ID."
+  );
+}
